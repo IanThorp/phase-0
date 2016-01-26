daaaa2c27b613299e60e3671fca880620773614f
Ian Thorp <IanMThorp@gmail.com>
2016-01-26 14:25:28 -0800

    comment out reflection for 5.2

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -78,26 +78,26 @@ end
 
 # 4. Reflection
 
-Were you successful in breaking the problem down into small steps?
+# Were you successful in breaking the problem down into small steps?
 
-  Yes, we were very successful breaking it down in pseudocode.
+#   Yes, we were very successful breaking it down in pseudocode.
 
-Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
+# Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
 
-  For the most part it was a pretty easy process. There were some small parts that were a little tough though.  The toughest part was figuring out how to translate make a new array that was a copy of the old array into code. We ended up just pointing to the same address at first, using new_array = old_array.  This did not work as well as we had hoped. We ended up with a long winded strategy of creating a new array, than taking each element of the old array and putting it in one at a time. This was a little ugly, but worked.
+#   For the most part it was a pretty easy process. There were some small parts that were a little tough though.  The toughest part was figuring out how to translate make a new array that was a copy of the old array into code. We ended up just pointing to the same address at first, using new_array = old_array.  This did not work as well as we had hoped. We ended up with a long winded strategy of creating a new array, than taking each element of the old array and putting it in one at a time. This was a little ugly, but worked.
 
-Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
+# Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
 
-  Initially, the problem was with our new_array = old_array command, which turns out is a shallow copy where we needed a deep copy. The difference between a shallow copy and a deep copy is something that I hadn't had to worry about in any code that I had written so far, so a shallow copy had always been sufficient.  This caused our non-destructive solution to actually be destructive and change the original variable. We ended up with a long winded strategy, as mentioned in the question above, which basically took each entry into the the old array and individually put it into the new array. This technically passed the tests but looked ugly.
+#   Initially, the problem was with our new_array = old_array command, which turns out is a shallow copy where we needed a deep copy. The difference between a shallow copy and a deep copy is something that I hadn't had to worry about in any code that I had written so far, so a shallow copy had always been sufficient.  This caused our non-destructive solution to actually be destructive and change the original variable. We ended up with a long winded strategy, as mentioned in the question above, which basically took each entry into the the old array and individually put it into the new array. This technically passed the tests but looked ugly.
 
-When you refactored, did you find any existing methods in Ruby to clean up your code?
+# When you refactored, did you find any existing methods in Ruby to clean up your code?
 
-  Oh yes!  The one the helped the most was the "new" method. I knew previously it could be used to make an empty array, but I did not know it could be used to copy one array into a different array. This cleaned up the code nicely.
+#   Oh yes!  The one the helped the most was the "new" method. I knew previously it could be used to make an empty array, but I did not know it could be used to copy one array into a different array. This cleaned up the code nicely.
 
-How readable is your solution? Did you and your pair choose descriptive variable names?
+# How readable is your solution? Did you and your pair choose descriptive variable names?
 
-  The solution is very readable.  Variable names are descriptive and the code only has necessary lines. It is pretty short to boot, which often helps improve readability.
+#   The solution is very readable.  Variable names are descriptive and the code only has necessary lines. It is pretty short to boot, which often helps improve readability.
 
-What is the difference between destructive and non-destructive methods in your own words?
+# What is the difference between destructive and non-destructive methods in your own words?
 
-  A non-destructive method can really be any kind of method, it just has a special relation to its destructive version. The destructive side of a method probably does something a little more powerful that requires caution. The easiest example is that a non-destructive method will create a new variable to hold the value of the action it performed, where as a destructive method will modify the existing variable. This is just an example, and each method can be very different.  Basically, a destructive method is labeled such as more of a "Heads up!", something potentially dangerous happens when you do this. Make sure you know what you are doing.
+#   A non-destructive method can really be any kind of method, it just has a special relation to its destructive version. The destructive side of a method probably does something a little more powerful that requires caution. The easiest example is that a non-destructive method will create a new variable to hold the value of the action it performed, where as a destructive method will modify the existing variable. This is just an example, and each method can be very different.  Basically, a destructive method is labeled such as more of a "Heads up!", something potentially dangerous happens when you do this. Make sure you know what you are doing.
